57890ef7e237081178a6bf49904417f5f1f75f54
sandhya063 <165256047+sandhya063@users.noreply.github.com>
2024-08-30 20:03:19 +1000

    added categories components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import Categories from "./components/Categories"
 function App() {
   
 
   return (
-   <>
-   </>
+    <div>
+   <Categories  title="Shop by Category" linkUrl="#" linkText="Browse all categories"/>
+   <Categories  title="New Categories" linkUrl="#" linkText="Browse all categories"/>
+   </div>
   )
 }
 
